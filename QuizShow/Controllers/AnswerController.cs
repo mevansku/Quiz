b3a67810cabcea28b42104dea081154aece85e0a
Mike Evans <40346092+mevansku@users.noreply.github.com>
2018-06-17 20:44:05 -0500

    Add files via upload
    start

---
+++ QuizShow/Controllers/AnswerController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuizShow.Models;
+
+namespace QuizShow.Controllers
+{
+    public class AnswerController : Controller
+    {
+        private QuizDB db = new QuizDB();
+
+        // GET: Answer
+        public ActionResult Index()
+        {
+            var answer = db.Answer.Include(a => a.Question);
+            return View(answer.ToList());
+        }
+
+        // GET: Answer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Answer answer = db.Answer.Find(id);
+            if (answer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(answer);
+        }
+
+        // GET: Answer/Create
+        public ActionResult Create()
+        {
+            ViewBag.QuestionId = new SelectList(db.Question, "QuestionId", "FilePath");
+            return View();
+        }
+
+        // POST: Answer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AnswerId,Text,CorrectFlag,QuestionId")] Answer answer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Answer.Add(answer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.QuestionId = new SelectList(db.Question, "QuestionId", "FilePath", answer.QuestionId);
+            return View(answer);
+        }
+
+        // GET: Answer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Answer answer = db.Answer.Find(id);
+            if (answer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.QuestionId = new SelectList(db.Question, "QuestionId", "FilePath", answer.QuestionId);
+            return View(answer);
+        }
+
+        // POST: Answer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AnswerId,Text,CorrectFlag,QuestionId")] Answer answer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(answer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.QuestionId = new SelectList(db.Question, "QuestionId", "FilePath", answer.QuestionId);
+            return View(answer);
+        }
+
+        // GET: Answer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Answer answer = db.Answer.Find(id);
+            if (answer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(answer);
+        }
+
+        // POST: Answer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Answer answer = db.Answer.Find(id);
+            db.Answer.Remove(answer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ QuizShow/Controllers/QuestionController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuizShow.Models;
+
+namespace QuizShow.Controllers
+{
+    public class QuestionController : Controller
+    {
+        private QuizDB db = new QuizDB();
+
+        // GET: Question
+        public ActionResult Index()
+        {
+            var question = db.Question.Include(q => q.Quiz);
+            return View(question.ToList());
+        }
+
+        // GET: Question/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Question.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // GET: Question/Create
+        public ActionResult Create()
+        {
+            ViewBag.QuizId = new SelectList(db.Quiz, "QuizId", "DisplayName");
+            return View();
+        }
+
+        // POST: Question/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "QuestionId,QuizId,FilePath,Question1,ActiveFlag")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Question.Add(question);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.QuizId = new SelectList(db.Quiz, "QuizId", "DisplayName", question.QuizId);
+            return View(question);
+        }
+
+        // GET: Question/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Question.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.QuizId = new SelectList(db.Quiz, "QuizId", "DisplayName", question.QuizId);
+            return View(question);
+        }
+
+        // POST: Question/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "QuestionId,QuizId,FilePath,Question1,ActiveFlag")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(question).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.QuizId = new SelectList(db.Quiz, "QuizId", "DisplayName", question.QuizId);
+            return View(question);
+        }
+
+        // GET: Question/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Question.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // POST: Question/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Question question = db.Question.Find(id);
+            db.Question.Remove(question);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
